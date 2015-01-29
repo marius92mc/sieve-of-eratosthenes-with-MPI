0ec98ac770b1d2f914cae9610c968d11ae6c3541
marius92mc <marius_92m@yahoo.com>
2015-01-29 23:24:07 +0200

    added example files

---
+++ example.c
@@ -0,0 +1,18 @@
+/* C Example */
+#include <stdio.h>
+#include <mpi.h>
+
+
+int main (argc, argv)
+     int argc;
+     char *argv[];
+{
+  int rank, size;
+
+  MPI_Init (&argc, &argv);	/* starts MPI */
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);	/* get current process id */
+  MPI_Comm_size (MPI_COMM_WORLD, &size);	/* get number of processes */
+  printf( "Hello world from process %d of %d\n", rank, size );
+  MPI_Finalize();
+  return 0;
+}
